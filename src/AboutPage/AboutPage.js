d731642ba9f0414d6f3cd4c7f051d9e358f8eeec
alshakerM <82781870+alshakerM@users.noreply.github.com>
2021-10-22 19:54:53 +0200

    add html css

---
+++ src/AboutPage/AboutPage.js
@@ -0,0 +1,23 @@
+import "./AboutPage.css";
+
+export function AboutPage() {
+  return (
+    <div className="body">
+      <div className="about-section">
+        <div className="inner-container">
+          <h1>About Me</h1>
+          <h2>Marwan Al Shaker</h2>
+          <p className="text">
+            im studying to be a javascript engineer. this is the app im
+            currently building as everything in life it's still a work in
+            progress. Hope you like experience
+          </p>
+          <div className="skill">
+            <span>Javascript Engineer</span>
+            <span>Front End Developer</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { HomePage } from "./HomePage";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import {AboutPage} from "./AboutPage/AboutPage"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reacto
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/about">
+          <AboutPage />
+          </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
