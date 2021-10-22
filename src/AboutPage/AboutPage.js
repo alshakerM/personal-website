17632f4e431f563321e2cb234338b5104d9dbc97
alshakerM <82781870+alshakerM@users.noreply.github.com>
2021-10-22 21:26:41 +0200

    add about page and nav buttons

--- src/AboutPage/AboutPage.js
+++ src/AboutPage/AboutPage.js
@@ -1,20 +1,36 @@
 import "./AboutPage.css";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Link } from "react-router-dom";
 
 export function AboutPage() {
   return (
     <div className="body">
-      <div className="about-section">
-        <div className="inner-container">
-          <h1>About Me</h1>
-          <h2>Marwan Al Shaker</h2>
-          <p className="text">
-            im studying to be a javascript engineer. this is the app im
-            currently building as everything in life it's still a work in
-            progress. Hope you like experience
-          </p>
-          <div className="skill">
-            <span>Javascript Engineer</span>
-            <span>Front End Developer</span>
+      <div className="about-containor">
+        <div className="about-sec-containor">
+          <div className="back-arrow"><Link to="/" style={{color:"white", opacity:"0.7"}}>
+          <ArrowBackIcon fontSize="large" /></Link></div>
+          <div className="content1">
+            <div className="text-area">
+              <p className="about-text-1">
+                Hi, i'm Marwan Al Shaker, a beginner Javascript engineer and
+                frontend developer, and currently working on improving myself
+              </p>
+              <p className="about-text-2">
+                Born in Baghdad, Iraq then i moved to Latakia, Syria in May of
+                2006 spent almost 10 years there the moved again to Colonge,
+                Germany in November 2015 and currently still there
+              </p>
+              <p className="about-text-3">
+                I have loved computers since i was a kid and was always fascinated by how they work although i didn't discover my passion into coding until recently.
+                always loved Gaming, and im a huge fan of cars. 
+              </p>
+            </div>
+            <div className="setup-sec">
+              <div className="pc">
+                <img src="https://pbs.twimg.com/profile_banners/1450213732748042243/1634593546/1500x500" alt="" className="pc-pic" />
+              </div>
+              <p className="pc-text">this is my pc that i built myself and designed this website on just thought it's cool to keep the meomory</p>
+            </div>
           </div>
         </div>
       </div>
