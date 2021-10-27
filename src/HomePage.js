ededec84e781c6df11664d1beae841b3578a1199
alshakerM <82781870+alshakerM@users.noreply.github.com>
2021-10-28 00:03:17 +0200

    edit cards

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './App.css';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import CardMedia from '@mui/material/CardMedia';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CardMedia from "@mui/material/CardMedia";
 
 const bull = (
   <Box
     component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
   >
     •
   </Box>
@@ -30,7 +30,7 @@ export function HomePage() {
             className="home-page-button about-button"
             to="/about"
             style={{
-              textDecoration: 'none',
+              textDecoration: "none",
             }}
           >
             About me
@@ -38,9 +38,9 @@ export function HomePage() {
           <button
             className="home-page-button follow-button"
             style={{
-              textDecoration: 'none',
+              textDecoration: "none",
             }}
-            onClick={() => window.open('https://twitter.com/m_alshaker/')}
+            onClick={() => window.open("https://twitter.com/m_alshaker/")}
           >
             Follow me on Twitter
           </button>
@@ -53,104 +53,100 @@ export function HomePage() {
             I have done some simple project, few examples:
           </p>
           <div className="card-container">
-            <Card
-              variant="outlined"
-              sx={{ width: 200 }}
-              style={{
-                margin: '10px 0',
-                backgroundColor: 'rgba(151, 151, 153, 0.1)',
-              }}
-              className="card"
-              onClick={() => window.open('https://trjwu.csb.app/')}
-            >
-              <CardContent>
-                <h3 className="title">Todo List App</h3>
-                <CardMedia
-                  className="project-img-todo"
-                  style={{ marginBottom: '20px' }}
-                  component="img"
-                  height="140"
-                  image="https://1.bp.blogspot.com/-i78iKr_P9Pk/X9ohjXyc5eI/AAAAAAAAA_4/8UauxZaOgUshGK7MXwW1gZqts7Zrf_AewCLcBGAsYHQ/s1280/Todo%2BList%2BApp%2Busing%2BHTML%2BCSS%2B%2526%2BJavaScript.webp"
-                  alt=""
-                />
-                <p className="card-text">
-                  A simple app that allows you to store actions list (todos) on
-                  a cloud.
-                </p>
-              </CardContent>
-              <CardActions>
-                <Button id="try" href="https://trjwu.csb.app/" target="_blank">
-                  Try it
-                </Button>
-              </CardActions>
-            </Card>
-            <Card
-              sx={{ width: 200 }}
-              style={{
-                margin: '10px 0',
-                backgroundColor: 'rgba(151, 151, 153, 0.1)',
-              }}
-              className="card"
-              onClick={() => window.open('https://6d0vu.csb.app/')}
-            >
-              <CardContent>
-                <h3 className="title">Movie Engine</h3>
-                <CardMedia
-                  className="project-img-movie"
-                  style={{ marginBottom: '20px' }}
-                  component="img"
-                  height="140"
-                  image="/Movie search.png"
-                  alt=""
-                />
-                <p className="card-text">
-                  A search engine app that allows you to seach for movie
-                  information from the huge database at imdb.com.
-                </p>
-              </CardContent>
-              <CardActions>
-                <Button id="try" href="https://6d0vu.csb.app/" target="_blank">
-                  Try it
-                </Button>
-              </CardActions>
-            </Card>
-            <Card
-              sx={{ width: 200 }}
-              style={{
-                margin: '10px 0',
-                backgroundColor: 'rgba(151, 151, 153, 0.1)',
-              }}
-              className="card"
-              onClick={() =>
-                window.open(
-                  'https://codesandbox.io/s/exciting-frost-f1grn?file=/src/LinkedList/LinkedList.js&fbclid=IwAR3CAYi0OXDSoyftIeTL_DapM91UR2Kx5sdEDtrT1uLSD9vVRHqLn0w40zY'
-                )
-              }
-            >
-              <CardContent>
-                <h3 className="title">Data Structures</h3>
-                <CardMedia
-                  className="project-img-data"
-                  style={{ marginBottom: '20px' }}
-                  component="img"
-                  height="140"
-                  image="https://media.springernature.com/w153/springer-static/cover/book/9781484239889.jpg"
-                  alt=""
-                />
-                <p className="card-text">
-                  A simple app that shows how Javascript data structures work.
-                </p>
-              </CardContent>
-              <CardActions>
-                <Button
-                  id="try"
-                  href="https://codesandbox.io/s/exciting-frost-f1grn?file=/src/LinkedList/LinkedList.js&fbclid=IwAR3CAYi0OXDSoyftIeTL_DapM91UR2Kx5sdEDtrT1uLSD9vVRHqLn0w40zY"
-                  target="_blank"
+            <ul>
+              <li>
+                <Card
+                  variant="outlined"
+                  sx={{ width: 200 }}
+                  style={{
+                    margin: "10px 0",
+                    backgroundColor: "rgba(151, 151, 153, 0.1)",
+                  }}
+                  className="card"
+                  onClick={() => window.open("https://trjwu.csb.app/")}
                 >
-                  Try it
-                </Button>
-              </CardActions>
-            </Card>
+                  <CardContent>
+                    <h3 className="title">Todo List App</h3>
+
+                    <p className="card-text">
+                      A simple app that allows you to store actions list (todos)
+                      on a cloud.
+                    </p>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      id="try"
+                      href="https://trjwu.csb.app/"
+                      target="_blank"
+                    >
+                      Try it
+                    </Button>
+                  </CardActions>
+                </Card>
+              </li>
+              <li>
+                <Card
+                  sx={{ width: 200 }}
+                  style={{
+                    margin: "10px 0",
+                    backgroundColor: "rgba(151, 151, 153, 0.1)",
+                  }}
+                  className="card"
+                  onClick={() => window.open("https://6d0vu.csb.app/")}
+                >
+                  <CardContent>
+                    <h3 className="title">Movie Engine</h3>
+
+                    <p className="card-text">
+                      A search engine app that allows you to seach for movie
+                      information from the huge database at imdb.com.
+                    </p>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      id="try"
+                      href="https://6d0vu.csb.app/"
+                      target="_blank"
+                    >
+                      Try it
+                    </Button>
+                  </CardActions>
+                </Card>
+              </li>
+              <li>
+                <Card
+                  sx={{ width: 200 }}
+                  style={{
+                    margin: "10px 0",
+                    backgroundColor: "rgba(151, 151, 153, 0.1)",
+                  }}
+                  className="card"
+                  onClick={() =>
+                    window.open(
+                      "https://codesandbox.io/s/exciting-frost-f1grn?file=/src/LinkedList/LinkedList.js&fbclid=IwAR3CAYi0OXDSoyftIeTL_DapM91UR2Kx5sdEDtrT1uLSD9vVRHqLn0w40zY"
+                    )
+                  }
+                >
+                  <CardContent>
+                    <h3 className="title">Data Structures</h3>
+
+                    <p className="card-text">
+                      A simple app that shows how Javascript data structures
+                      work.
+                    </p>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      id="try"
+                      href="https://codesandbox.io/s/exciting-frost-f1grn?file=/src/LinkedList/LinkedList.js&fbclid=IwAR3CAYi0OXDSoyftIeTL_DapM91UR2Kx5sdEDtrT1uLSD9vVRHqLn0w40zY"
+                      target="_blank"
+                    >
+                      Try it
+                    </Button>
+                  </CardActions>
+                </Card>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
